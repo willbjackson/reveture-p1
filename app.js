00c33d09e2e5b96c347504c7745648e0566a05b9
jackson218 <wbjackson218@gmail.com>
2019-05-06 16:06:42 -0500

    added script

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cosmos = require('@azure/cosmos').CosmosClient;
 const app = express()
 const port = 8080
-const multer  = require('multer')
+const multer = require('multer')
 const {
   Aborter,
   BlockBlobURL,
@@ -14,18 +14,35 @@ const {
   uploadStreamToBlockBlob
 } = require('@azure/storage-blob');
 
+// https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-nodejs-v10
 const inMemoryStorage = multer.memoryStorage();
 const uploadStrategy = multer({ storage: inMemoryStorage }).single('image');
 const getStream = require('into-stream');
 const containerName = 'images';
+
+// The next set of constants helps to reveal the intent of file size calculations during upload operations.
+
 const ONE_MEGABYTE = 1024 * 1024;
 const uploadOptions = { bufferSize: 4 * ONE_MEGABYTE, maxBuffers: 20 };
+
+// Requests made by the API can be set to time-out after a given interval. The Aborter class is responsible for managing how requests are timed-out and the following constant is used to define timeouts used in this sample.
+
 const ONE_MINUTE = 60 * 1000;
+
+// Account credentials are used to create a pipeline, which is responsible for managing how requests are sent to the REST API. Pipelines are thread-safe and specify logic for retry policies, logging, HTTP response deserialization rules, and more.
+
+// The SharedKeyCredential class is responsible for wrapping storage account credentials to provide them to a request pipeline.
 
 const sharedKeyCredential = new SharedKeyCredential(
   process.env.AZURE_STORAGE_ACCOUNT_NAME,
   process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY);
+
+// The StorageURL class is responsible for creating a new pipeline.
+
 const pipeline = StorageURL.newPipeline(sharedKeyCredential);
+
+// The ServiceURL models a URL used in the REST API. Instances of this class allow you to perform actions like list containers and provide context information to generate container URLs.
+
 const serviceURL = new ServiceURL(
   `https://${process.env.AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net`,
   pipeline
@@ -34,33 +51,34 @@ const serviceURL = new ServiceURL(
 const getBlobName = originalName => {
   // Use a random number to generate a unique file name, 
   // removing "0." from the start of the string.
-  const identifier = Math.random().toString().replace(/0\./, ''); 
+  const identifier = Math.random().toString().replace(/0\./, '');
   return `${identifier}-${originalName}`;
 };
 
 app.post('/profile', uploadStrategy, async (req, res) => {
-    const aborter = Aborter.timeout(30 * ONE_MINUTE);
-    const blobName = getBlobName(req.file.originalname);
-    const stream = getStream(req.file.buffer);
-    const containerURL = ContainerURL.fromServiceURL(serviceURL, containerName);
-    const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, blobName);
-    
-    
-    try {
-        await uploadStreamToBlockBlob(aborter, stream,
-            blockBlobURL, uploadOptions.bufferSize, uploadOptions.maxBuffers);
-            res.redirect("/")
-            
-        } catch (err) {
-            res.json(err)
+  const aborter = Aborter.timeout(30 * ONE_MINUTE);
+  const blobName = getBlobName(req.file.originalname);
+  const stream = getStream(req.file.buffer);
+  const containerURL = ContainerURL.fromServiceURL(serviceURL, containerName);
+  const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, blobName);
+
+
+  try {
+    await uploadStreamToBlockBlob(aborter, stream,
+      blockBlobURL, uploadOptions.bufferSize, uploadOptions.maxBuffers);
+    res.redirect("/")
+
+  } catch (err) {
+    res.json(err)
 
   }
 });
 
-
-const nosql = new cosmos( {endpoint: process.env.AZURE_COSMOS_URI, auth: { 
-  masterKey: process.env.AZURE_COSMOS_PRIMARY_KEY
-}});
+const nosql = new cosmos({
+  endpoint: process.env.AZURE_COSMOS_URI, auth: {
+    masterKey: process.env.AZURE_COSMOS_PRIMARY_KEY
+  }
+});
 
 nosql.database('nac').container('jh').items.readAll().toArrary().then(res => console.log(res.result))
 
---
+++ script.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+groupName=$1
+storageAccountName=$2
+location=southcentralus
+servicePlanName=$3
+appName=$4
+gitrepo=https://github.com/willbjackson/reveture-p1
+token=17396aedab1b72aad408bb61924f738ce9070ef7
+cosmosName=$5
+cosmosDatabaseName=$6
+cosmosContainerName=$7
+
+# Create a resource group.
+az group create --location $location --name $groupName
+
+az storage account create \
+    --name $storageAccountName \
+    --location $location \
+    --resource-group $groupName \
+    --kind blobstorage \
+    --sku Standard_LRS \
+    --access-tier hot
+
+blobStorageAccountKey=$(az storage account keys list -g $groupName \
+-n $storageAccountName --query [0].value --output tsv)
+
+az storage container create -n images --account-name $storageAccountName \
+--account-key $blobStorageAccountKey --public-access off
+
+echo $blobStorageAccountKey
+
+
+# Create a SQL API Cosmos DB account with session consistency and multi-master enabled.
+az cosmosdb create \
+    --resource-group $groupName \
+    --name $cosmosName \
+    --kind GlobalDocumentDB \
+    --locations "South Central US"=0 "North Central US"=1 \
+    --default-consistency-level "Session" \
+    --enable-multiple-write-locations true
+
+
+# Create a database
+az cosmosdb database create \
+    --resource-group $groupName \
+    --name $cosmosName \
+    --db-name $cosmosDatabaseName
+
+
+# Create a SQL API container with a partition key and 1000 RU/s.
+az cosmosdb collection create \
+    --resource-group $groupName \
+    --collection-name $cosmosContainerName \
+    --name $cosmosName \
+    --db-name $cosmosDatabaseName \
+    --partition-key-path /mypartitionkey \
+    --throughput 1000
+
+# Create an App Service 
+az appservice plan create --name $servicePlanName --resource-group $groupName --sku B1 --location $location --is-linux
+
+# Create a web app.
+az webapp create --resource-group $groupName --plan $servicePlanName --name $appName -r "node|10.14"
+
+az webapp config appsettings set -g $groupName -n $appName --settings AZURE_STORAGE_ACCOUNT_NAME=$appName
+az webapp config appsettings set -g $groupName -n $appName --settings AZURE_STORAGE_ACCOUNT_ACCESS_KEY=$blobStorageAccountKey
+
+# Configure continuous deployment from GitHub. 
+# --git-token parameter is required only once per Azure account (Azure remembers token).
+az webapp deployment source config --name $appName --resource-group $groupName \
+--repo-url $gitrepo --branch master --git-token $token
+
+# add instances 
+az appservice plan update -g $groupName -n $servicePlanName --number-of-workers 3
+
+primaryKey=$(az cosmosdb list-keys --name willdb218 -g wjgroup218 --query primaryMasterKey -o tsv)
+
+az webapp config appsettings set -g $groupName -n $appName --settings AZURE_COSMOS_URI=https://${cosmosName}.documents.azure.com:443/
+az webapp config appsettings set -g $groupName -n $appName --settings AZURE_COSMOS_PRIMARY_KEY=$primaryKey
